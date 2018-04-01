c455723e853e7617fd7562d3519ed4bc54a5c629
nothingismagick <nothing@magick>
2018-04-02 00:48:03 +0200

    First Commit - weee

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const IPFS = require('ipfs')
+const OrbitDB = require('orbit-db')
+
+const ipfsOptions = {
+    EXPERIMENTAL: {
+        pubsub: true
+    }
+}
+
+// Create IPFS instance
+const ipfs = new IPFS(ipfsOptions)
+
+ipfs.on('ready', () => {
+    // Create OrbitDB instance
+    const orbitdb = new OrbitDB(ipfs)
+})
+
+const app = express()
+app.use(cors())
+
+app.use('/', express.static(__dirname))
+
+
+app.listen(443, function () {
+  console.log('Listening on port 443.');
+});
